5c075eaa33300dbd5e4242edf9ac456b56c62cca
Salvatore Calo <salvatorecalo@MacBook-Pro-di-Salvatore.local>
2025-01-17 19:55:57 +0100

    fix: now the program run in every folder you put the exe

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ import os
 from plugins.load_extension import load_extension
 from plugins.generate_folder import generate_folder
 from plugins.organize_files import organize_files
+import sys 
 
 def main():
-    with open("log_file.txt", "w") as log_file:
+    if getattr(sys, 'frozen', False):
+        os.chdir(os.path.dirname(sys.executable))
+        script_path = os.path.abspath(__file__)
+    else:
+        os.chdir(os.path.dirname(__file__))
+        script_path = os.path.abspath(__file__)
+    with open(f"{os.path.curdir}/log_file.txt", "w") as log_file:
         print("log file created")
         files = os.listdir()
-        script_path = os.path.abspath(__file__)
         extension = load_extension(files, log_file, script_path)
         generate_folder(extension, log_file)
         organize_files(files, script_path, log_file)
